e0ad086fc8889d42bf104b16a216e7658e0200e0
AValerievna <a.valerievna12@gmail.com>
2019-01-23 13:33:59 +0300

    regexp fixies

--- BaseJsTask/project/find_diff_words.js
+++ BaseJsTask/project/find_diff_words.js
@@ -3,9 +3,10 @@ let jsonData = require('../resources/data.json');
 let str1 = jsonData.firstStringToCompare;
 let str2 = jsonData.secondStringToCompare;
 
-const wordRegExp = /\b[a-zA-Zа-яА-Я]+(\d+)?([-`][a-zA-Zа-яА-Я]+(\d+))?\b/;
+const wordRegExp = /\b[a-zA-Zа-яА-Я]+(\d*)?([-`][a-zA-Zа-яА-Я]+(\d*))?\b/g;
 
 let strArr2 = str2.toLocaleLowerCase().match(wordRegExp);
+console.log(strArr2);
 let res = str1.toLowerCase().match(wordRegExp).filter(el => !strArr2.includes(el));
 console.log(`Words of first string, different from second-string words:\n${res}`);
 
