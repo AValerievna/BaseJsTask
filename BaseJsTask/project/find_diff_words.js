ef97f53244b44e436010a09842737fd6c6b5eebb
AValerievna <a.valerievna12@gmail.com>
2019-01-23 13:44:13 +0300

    regexp fixies

--- BaseJsTask/project/find_diff_words.js
+++ BaseJsTask/project/find_diff_words.js
@@ -3,7 +3,7 @@ let jsonData = require('../resources/data.json');
 let str1 = jsonData.firstStringToCompare;
 let str2 = jsonData.secondStringToCompare;
 
-const wordRegExp = /\b[a-zA-Zа-яА-Я]+(\d*)?([-`][a-zA-Zа-яА-Я]+(\d*))?\b/g;
+const wordRegExp = /\b[a-zA-Zа-яА-Я]+\d*([-`][a-zA-Zа-яА-Я]+\d*)?\b/g;
 
 let strArr2 = str2.toLocaleLowerCase().match(wordRegExp);
 console.log(strArr2);
